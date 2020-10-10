16b036912c162e2f022110e9f0e774a0dbc9c714
Patel210 <pranavpatel2101997@gmail.com>
2020-10-10 16:45:23 +0530

    Added: Refactor1 Exceution:Successful

--- src/main/java/com/capgemini/findmaxgenerics/FindMaximum.java
+++ src/main/java/com/capgemini/findmaxgenerics/FindMaximum.java
@@ -3,46 +3,20 @@ package com.capgemini.findmaxgenerics;
 public class FindMaximum {
 
 	/**
-	 * Returns maximum of 3 integers
+	 * @param <E>
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return Maximum of 3 parameters
 	 */
-	public Integer max(Integer integer1, Integer integer2, Integer integer3) {
+	public <E extends Comparable<E>> E max(E a, E b, E c) {
 
-		Integer max = integer1;
-		if (integer2.compareTo(max) > 0) {
-			max = integer2;
+		E max = a;
+		if (b.compareTo(max) > 0) {
+			max = b;
 		}
-		if (integer3.compareTo(max) > 0) {
-			max = integer3;
-		}
-		return max;
-	}
-
-	/**
-	 * Returns maximum of 3 integers
-	 */
-	public Float max(Float float1, Float float2, Float float3) {
-
-		Float max = float1;
-		if (float2.compareTo(max) > 0) {
-			max = float2;
-		}
-		if (float3.compareTo(max) > 0) {
-			max = float3;
-		}
-		return max;
-	}
-
-	/**
-	 * Returns maximum of 3 strings (lexicographically)
-	 */
-	public String max(String string1, String string2, String string3) {
-
-		String max = string1;
-		if (string2.compareTo(max) > 0) {
-			max = string2;
-		}
-		if (string3.compareTo(max) > 0) {
-			max = string3;
+		if (c.compareTo(max) > 0) {
+			max = c;
 		}
 		return max;
 	}
